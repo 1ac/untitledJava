5611185eba14d4137c16bba30a4447c902e3ce67
1ac <43090940+1ac@users.noreply.github.com>
2021-09-12 23:18:43 +0300

    Update

--- JavaByExample/src/main/java/Part1/Charpter2/CopmlexNumber.java
+++ JavaByExample/src/main/java/Part1/Charpter2/CopmlexNumber.java
@@ -1,4 +1,29 @@
 package Part1.Charpter2;
 
+/**
+ * Класс представляет комплексные числа и определяет методы, реализующие
+ * арифметику комплексных чисел
+ */
+
 public class CopmlexNumber {
+    /** Переменная экземпляра.
+     * Каждый объект класса ComplexNumber содержит два значения типа double
+     * Они описаны как private и недоступны извне этого класса
+     * Доступ к ним осуществляется посредством методов real() imaginary()
+     */
+
+    private double x, y;
+
+    //Это конструктор он инициализирует перменные x и y
+    public CopmlexNumber(double real, double imaginary) {
+        this.x = real;
+        this.y = imaginary;
+    }
+
+    /**
+     * Метод достпа. Возвращает действительную часть комплексного числа.
+     * Отсутсвие метода setReal() означает что класс ComplexNumber является "ytbpvtyztvsv"
+     */
+
+    public double real
 }
